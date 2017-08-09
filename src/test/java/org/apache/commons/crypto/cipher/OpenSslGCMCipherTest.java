bff3ee431efb5ae5673e7cce2dc8946b9198f50c
jmclaughlin <james.mclaughlin@imanage.com>
2017-08-08 19:53:18 -0500

    Encrypt / Decrypt test for GCM

--- src/test/java/org/apache/commons/crypto/cipher/OpenSslGCMCipherTest.java
+++ src/test/java/org/apache/commons/crypto/cipher/OpenSslGCMCipherTest.java
@@ -4,7 +4,12 @@ import org.junit.Assert;
 import org.junit.Assume;
 import org.junit.Test;
 
+import javax.crypto.*;
+import javax.crypto.spec.GCMParameterSpec;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
 public class OpenSslGCMCipherTest {
 
@@ -19,5 +24,42 @@ public class OpenSslGCMCipherTest {
             Assert.fail ("AES/GCM/NoPadding Not Supported");
             throw e;
         }
+    }
+
+    @Test
+    public void testEncrypt () throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, BadPaddingException, ShortBufferException, IllegalBlockSizeException {
+        OpenSsl enc = OpenSsl.getInstance("AES/GCM/NoPadding");
+
+        OpenSsl dec = OpenSsl.getInstance ("AES/GCM/NoPadding");
+
+        KeyGenerator keyGenerator = KeyGenerator.getInstance ("AES");
+        keyGenerator.init (256);
+        SecretKey key = keyGenerator.generateKey ();
+
+        byte[] nonce = new byte[12];
+
+        SecureRandom secureRandom = new SecureRandom ();
+
+        secureRandom.nextBytes (nonce);
+
+        GCMParameterSpec gcmParameters = new GCMParameterSpec (96, nonce);
+
+        enc.init (OpenSsl.ENCRYPT_MODE, key.getEncoded (), gcmParameters);
+
+        dec.init (OpenSsl.DECRYPT_MODE, key.getEncoded (), gcmParameters);
+
+        byte[] plainText = "covfefe".getBytes (StandardCharsets.UTF_8);
+
+        byte[] cipherText = new byte[plainText.length + 12];
+
+        enc.doFinal (plainText, 0, plainText.length, cipherText, 0);
+
+        byte[] decryptedText = new byte[plainText.length];
+
+        dec.doFinal (cipherText, 0, cipherText.length, decryptedText, decryptedText.length);
+
+        Assert.assertArrayEquals (plainText, decryptedText);
+
+
     }
 }
